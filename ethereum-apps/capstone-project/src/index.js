51cb5b947ef6bd8efae1f988edd4021d919dfab6
C840985 <NareshKumar.Kakumani@BNSF.com>
2020-01-11 19:50:25 -0600

    app.js changes

--- ethereum-apps/capstone-project/src/index.js
+++ ethereum-apps/capstone-project/src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css'
 
 import App from './components/App';
+// import * as serviceWorker from ./serviceWorker.js
 
 ReactDOM.render(<App />, document.querySelector('#root'));
+
+// If you want your app to work offilne and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers. https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
